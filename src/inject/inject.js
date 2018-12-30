69cc700bc639b55671170f8900ec02bddf7a8208
git-uname <a.french@mail.utoronto.ca>
2018-12-30 14:42:20 -0500

    Update warning hiding
    Hide private profile warning
    Remove margin where warnings go after removing warnings

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,6 +4,11 @@ if (window.location.href == "https://tf2center.com/") {
 
 window.noAds = true;
 $(".youShallNotPassEnabled").removeClass("youShallNotPassEnabled")
-$('.join-steam-group').parent().parent().hide();
+// Remove warnings
+$('.join-steam-group').parent().parent().remove();
+$('.verifySteamProfile').parent().parent().remove();
+$('a[href="./faq#adblock"]').remove();
+$('.contentWrapper').first().removeAttr('style'); // Remove space for warnings
+// Remove existing click handlers which we will replace
 $('.playerTopMenu').prop('onclick', null).off('click');
 $('.trigger').prop('onclick', null).off('click');
